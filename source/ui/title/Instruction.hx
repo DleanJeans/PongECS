c92b24a1a2eaa513629c8677f00b995272129f8f
Dlean Jeans <dleanjeans@gmail.com>
2016-12-23 23:15:12 +0700

    Update Instruction

--- source/ui/title/Instruction.hx
+++ source/ui/title/Instruction.hx
@@ -7,7 +7,7 @@ import flixel.util.FlxAxes;
 class Instruction extends FlxText {
 	public function new() {
 		super(0, FlxG.height - 125, Settings.playField.width,
-		"A or Left Arrow to Move Left\nD or Right Arrow to Move Right\n\nBy Dlean Jeans\n@DleanJeans", 15);
+		'Score ${Settings.scoreToWin} points to win\nMove Left: A/Left Arrow\nMove Right: D/Right Arrow\n\nBy Dlean Jeans\n@DleanJeans', 15);
 		alignment = FlxTextAlign.CENTER;
 		screenCenter(FlxAxes.X);
 	}
