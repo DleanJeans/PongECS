6d4a7029c71a4f5a3dec42ab4a003c1bca7dc1b4
Dlean Jeans <dleanjeans@gmail.com>
2016-12-25 18:19:38 +0700

    Simplize MenuButton

--- source/ui/gameplay/MenuButton.hx
+++ source/ui/gameplay/MenuButton.hx
@@ -7,44 +7,16 @@ import flixel.util.FlxColor;
 import ui.Button;
 
 class MenuButton extends Button {
-	public static var MENU = "MENU";
-	public static var CLICK_AGAIN = if (FlxG.onMobile) "TAP AGAIN" else "CLICK AGAIN";
-	
 	public function new() {
-		super(5, 0, MENU, onClick, Settings.leftSpace.width - 10);
+		super(5, 0, "MENU", backToMenu, Settings.leftSpace.width - 10);
 		setupLabel();
 		UI.hide(this);
-		
-		FlxMouseEventManager.add(this, null, null, null, resetOnMouseLeaving, false, true,
-		false); // `pixelPerfect = false` make button with transparent graphic clickable
 	}
 	
 	public function setupLabel() {
 		var landscape = Settings.landscape;
-		label.wordWrap = landscape;
 		label.autoSize = !landscape;
 		label.alignment = landscape ? FlxTextAlign.CENTER : FlxTextAlign.LEFT;
-	}
-	
-	function resetOnMouseLeaving(_) {
-		text = MENU;
-		updateTextBox();
-	}
-	
-	function onClick() {
-		if (text == MENU) {
-			text = CLICK_AGAIN;
-		}
-		else if (text == CLICK_AGAIN) {
-			text = MENU;
-			backToMenu();
-		}
-		updateTextBox();
-	}
-	
-	function updateTextBox() {
-		setGraphicSize(cast label.width, cast label.height);
-		updateHitbox();
 	}
 	
 	function backToMenu() {
