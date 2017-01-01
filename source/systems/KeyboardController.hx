c6d8957bf06faeb2960f50a167a2f1dd5b7d161c
Dlean Jeans <dleanjeans@gmail.com>
2017-01-01 16:32:34 +0700

    KeyboardController: Add 2-player keyboard controller

--- source/systems/KeyboardController.hx
+++ source/systems/KeyboardController.hx
@@ -1,18 +1,53 @@
 package systems;
 
+import components.Direction;
 import components.PlayerControlled;
+import edge.Entity;
 import edge.ISystem;
 import flixel.FlxG;
 import flixel.FlxSprite;
 
 class KeyboardController implements ISystem {
-	public function update(sprite:FlxSprite, player:PlayerControlled) {
+	public function update(sprite:FlxSprite, player:PlayerControlled, team:Entity) {
 		#if !FLX_NO_KEYBOARD
+		if (Settings.splitScreen)
+			updateTwoPlayer(sprite, player, team);
+		else updateOnePlayer(sprite, player, team);
+		#end
+	}
+	
+	function updateOnePlayer(sprite:FlxSprite, player:PlayerControlled, team:Entity) {
 		player.movingDirection = 0;
 		if (FlxG.keys.anyPressed([A, LEFT]))
-			player.movingDirection += -1;
+			moveLeft(player);
 		if (FlxG.keys.anyPressed([D, RIGHT]))
-			player.movingDirection += 1;
-		#end
+			moveRight(player);
+	}
+	
+	function updateTwoPlayer(sprite:FlxSprite, player:PlayerControlled, team:Entity) {
+		var teamPosition = team.get(Direction).direction;
+		
+		player.movingDirection = 0;
+		
+		switch (teamPosition) {
+			case Direction.DOWN:
+				if (FlxG.keys.pressed.A)
+					moveLeft(player);
+				else if (FlxG.keys.pressed.D)
+					moveRight(player);
+			case Direction.UP:
+				if (FlxG.keys.pressed.LEFT)
+					moveLeft(player);
+				else if (FlxG.keys.pressed.RIGHT)
+					moveRight(player);
+		}
+	}
+	
+	function moveLeft(player:PlayerControlled) {
+		player.movingDirection -= 1;
+	}
+	
+	function moveRight(player:PlayerControlled) {
+		player.movingDirection += 1;
 	}
 }
